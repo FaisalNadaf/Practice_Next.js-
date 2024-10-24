8c967ce6fded71e4db7e0d924d1043e30486200f
FaisalNadaf <faisalfnadaf@gmail.com>
2024-10-24 23:25:44 +0530

    code

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,37 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
-  return (
-   <><div className="flex h-screen w-screen items-center justify-center">hello</div></>
-  );
+export default function Home() 
+{
+	const route = useRouter();
+	return (
+		<>
+			<div className="flex h-screen w-screen items-center justify-center">
+				<button
+					onClick={() => {
+						route.push("/Pages/homePage");
+					}}
+					className="m-2 px4 py-2 bg-gray-400 w-48 h-18 border rounded-xl"
+				>
+					home
+				</button>
+				<button
+					onClick={() => {
+						route.push("/Pages/contactPage");
+					}}
+					className="m-2 px4 py-2 bg-gray-400 w-48 h-18 border rounded-xl"
+				>
+					contact
+				</button>
+				<button
+					onClick={() => {
+						route.push("/Pages/loginPage");
+					}}
+					className="m-2 px4 py-2 bg-gray-400 w-48 h-18 border rounded-xl"
+				>
+					login
+				</button>
+			</div>
+		</>
+	);
 }
